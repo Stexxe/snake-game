c4e6a26fc19a5fc0a33a91fe1a757cd5401496a9
Aleksei Tirman <aleksei.tirman@gmail.com>
2019-06-22 13:37:59 +0300

    Init

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const path = require('path');
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'bundle.js',
+  },
+  mode: 'development',
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
+      },
+    ],
+  },
+  stats: {
+    colors: true,
+  },
+  devtool: 'source-map',
+};
